b2d651ac6a9f8bae5e4a59777e4acbcff9fefc9b
Andrew Jesson <andrew@tensorzero.com>
2026-02-13 16:16:41 -0500

    update bindings

--- internal/autopilot-worker/src/wrapper.rs
+++ internal/autopilot-worker/src/wrapper.rs
@@ -369,7 +369,7 @@ mod tests {
         CreateDatapointsResponse, DeleteDatapointsResponse, FeedbackParams, FeedbackResponse,
         GetConfigResponse, GetDatapointsResponse, GetInferencesRequest, GetInferencesResponse,
         InferenceResponse, ListDatapointsRequest, ListDatasetsRequest, ListDatasetsResponse,
-        ListEpisodesParams, ListEpisodesResponse, ListInferencesRequest, UpdateDatapointRequest,
+        ListEpisodesRequest, ListEpisodesResponse, ListInferencesRequest, UpdateDatapointRequest,
         UpdateDatapointsResponse, WriteConfigRequest, WriteConfigResponse,
     };
     use tensorzero_core::config::snapshot::SnapshotHash;
@@ -483,7 +483,7 @@ mod tests {
 
             async fn list_episodes(
                 &self,
-                params: ListEpisodesParams,
+                request: ListEpisodesRequest,
             ) -> Result<ListEpisodesResponse, TensorZeroClientError>;
 
             async fn launch_optimization_workflow(
